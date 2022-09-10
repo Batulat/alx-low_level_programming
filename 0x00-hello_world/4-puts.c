6f5d68bfc2c7e6183577be9231e65e48a49afb20
Batulat <111013374+Batulat@users.noreply.github.com>
2022-09-10 04:53:33 -0700

    "Programming is like building a multilingual puzzle displayed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/*
+ * main - Entry point
+ * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line".
+ */
+#include<stdio.h>
+#include<stlib.h>
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+}
