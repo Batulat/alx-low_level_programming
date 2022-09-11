2f21aadc5e5fcca9b6aebc7a8fd22fdb098bd74e
Batulat <111013374+Batulat@users.noreply.github.com>
2022-09-11 07:08:13 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,9 @@
 #include<unistd.h>
 /**
  * main - The entry point
- * Return: 1 when 
+ * Return: 1 when
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
