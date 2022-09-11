0114b37346e636a9a71812ac37e3d35e4d59150e
Batulat <111013374+Batulat@users.noreply.github.com>
2022-09-11 03:36:10 -0700

    6-size.c correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,21 @@
 /**
  * main - The entry point of printf
  * printf: displays size of each data types
- * @c: rep char
- * @i: rep integer
- * @s: rep short
- * @f: rep float
+ * c: rep char
+ * i: rep integer
+ * s: rep short
+ * f: rep float
  * Return: 0
  */
-int main(char c, int i, short s, float f)
+int main(void)
 {
-	printf("Size of a char: %ld byte(s)\n", sizeof(char));
-	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	char c;
+	int i;
+	short s;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld bytes(s)\n", sizeof(float));
