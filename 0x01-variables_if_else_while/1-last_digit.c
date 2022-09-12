179ac08b4c906283a87fec21ffa16f135d39dbcc
Batulat <111013374+Batulat@users.noreply.github.com>
2022-09-12 14:32:49 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,9 +24,5 @@ int main(void)
 	{
 		printf("Last digit of %i is %d and is 0\n", n, a);
 	}
-	else (a < 6, a != 0);
-	{
-		printf("Last digit of %i is %d  and is less than 6 and not 0\n", n, a);
-	}
 	return (0);
 }
