316b7f6b00032619510effe347ea0664d51d6574
Batulat <111013374+Batulat@users.noreply.github.com>
2022-09-12 17:56:18 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry
+ * description: display of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+
+	{
+		printf("%i\n", i);
+	}
+	return (0);
+}
