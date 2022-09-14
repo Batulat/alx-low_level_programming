5292ce300f223f3ae6686b0851d2c1e2b4b12962
Batulat <111013374+Batulat@users.noreply.github.com>
2022-09-14 09:28:12 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_alphabet - Entry point
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	return (0);
+}
