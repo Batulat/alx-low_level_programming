b6e739b48ee7bd74b4957d769b0f923ead277670
Batulat <111013374+Batulat@users.noreply.github.com>
2022-09-14 12:45:42 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.h"
+/**
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (c = 1; c <= 10; c++)
+
+	{
+		for (i = 'a'; i < 'z'; i++)
+
+		{
+			_putchar(c);
+		}
+	}
+}
