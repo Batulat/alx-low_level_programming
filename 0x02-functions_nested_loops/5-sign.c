0c94c51b8a9590e96c01b385ddcfc2174a677a4f
Batulat <111013374+Batulat@users.noreply.github.com>
2022-09-14 20:21:44 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_sign - entry
+ * Return: 1 when success
+ * @c: signs
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
